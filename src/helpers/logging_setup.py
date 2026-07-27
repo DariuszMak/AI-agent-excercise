fae4150c9066fdd004a62456d60b43f27bf853b6
Dariusz <darekmakarewicz1994@gmail.com>
2026-07-27 07:32:40 +0200

    add changes

--- src/helpers/logging_setup.py
+++ src/helpers/logging_setup.py
@@ -1,16 +1,12 @@
+import os
 import logging
-
 import structlog
 
-from src.helpers.config.config import Config
-
-
-def logging_setup(
-    level: int = logging.INFO,
-    log_file: str | None = None,
-) -> None:
-    log_file = log_file or Config().log_file
-
+def logging_setup(level: int = logging.INFO, log_file: str | None = None) -> None:
+    is_docker = os.getenv("LOG_TO_FILE", "true").lower() == "false"
+    if is_docker:
+        log_file = None
+    
     root_logger = logging.getLogger()
     root_logger.setLevel(level)
     root_logger.handlers.clear()
@@ -27,13 +23,13 @@ def logging_setup(
     console_handler = logging.StreamHandler()
     console_handler.setLevel(level)
     console_handler.setFormatter(formatter)
-
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(level)
-    file_handler.setFormatter(formatter)
-
     root_logger.addHandler(console_handler)
-    root_logger.addHandler(file_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+        root_logger.addHandler(file_handler)
 
     structlog.configure(
         processors=[
@@ -41,16 +37,10 @@ def logging_setup(
             structlog.stdlib.filter_by_level,
             structlog.stdlib.add_log_level,
             structlog.stdlib.add_logger_name,
-            structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
-    )
-
-    structlog.contextvars.bind_contextvars(
-        service="python_gui",
-        env="dev",
     )
