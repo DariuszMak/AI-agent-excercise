154fd8b27655c9f67bedaa6e2c674d674f5e0383
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-07-28 14:55:29 +0200

    update setup

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -3,6 +3,4 @@
 uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "not slow" ; 
 uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "slow" ; 
 
-uv run pytest tests/eval/test_rag_accuracy.py ;  
-
 Start-Process .\htmlcov\index.html ; 
